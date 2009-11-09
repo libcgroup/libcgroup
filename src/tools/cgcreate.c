0c7d6e8daafbdaa699c6dc4b2d0dce958064d84b
Ivana Hutarova Varekova <varekova@redhat.com>
2009-11-09 11:38:14 +0100

    Add copyright header to cgcreate
    Signed-off-by: Ivana Hutarova Varekova <varekova@redhat.com>
    Signed-off-by: Dhaval Giani <dhaval@linux.vnet.ibm.com>

--- src/tools/cgcreate.c
+++ src/tools/cgcreate.c
@@ -1,3 +1,18 @@
+/*
+ * Copyright Red Hat, Inc. 2009
+ *
+ * Authors:	Ivana Hutarova Varekova <varekova@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2.1 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
 #include <libcgroup.h>
 #include <libcgroup-internal.h>
 
