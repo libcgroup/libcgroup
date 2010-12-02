240b5ed20f10170df3548b9a444096d631019c44
Jan Safranek <jsafrane@redhat.com>
2010-12-02 15:00:40 +0100

    There is a bug which causes the initialised configuration will be bogus or does not work at all and majority of tools does not work well. The situation happens when multiple mount points are mounted for one hierarchy and the hierarchy have multiple controllers:
    example:
    
    # lssubsys
    cpu,cpuacct /mnt/cgroups/cpu
    cpuacct /cgroup/cpu
    memory /cgroup/memory
    
    # cat /proc/mounts | grep cgroup
    cpu /mnt/cgroups/cpu cgroup rw,relatime,cpuacct,cpu 0 0
    cgroup /cgroup/cpu cgroup rw,relatime,cpuacct,cpu 0 0
    cgroup /cgroup/memory cgroup rw,relatime,memory 0 0
    
    
    
    Signed-off-by: Ivana Hutarova Varekova <varekova@redhat.com>
    Acked-by: Jan Safranek <jsafrane@redhat.com>
    Signed-off-by: Jan Safranek <jsafrane@redhat.com>

--- src/api.c
+++ src/api.c
@@ -826,7 +826,7 @@ int cgroup_init(void)
 			if (duplicate) {
 				cgroup_dbg("controller %s is already mounted on %s\n",
 					mntopt, cg_mount_table[j].path);
-				continue;
+				break;
 			}
 
 			strcpy(cg_mount_table[found_mnt].name, controllers[i]);
