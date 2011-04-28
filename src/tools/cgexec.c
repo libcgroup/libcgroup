19c221a82758291b81d945de3f9cf00639d7469a
Ivana Hutarova Varekova <varekova@redhat.com>
2011-04-28 10:25:17 +0200

    Add -h option to cgexec tool
    This patch adds option -h to cgexec tool
    
    Signed-off-by: Ivana Hutarova Varekova <varekova@redhat.com>
    Signed-off-by: Jan Safranek <jsafrane@redhat.com>

--- src/tools/cgexec.c
+++ src/tools/cgexec.c
@@ -35,9 +35,24 @@
 #include "tools-common.h"
 
 static struct option longopts[] = {
-	{"sticky", no_argument, NULL, 's'}, 
+	{"sticky", no_argument, NULL, 's'},
+	{"help", no_argument, NULL, 'h'},
 	{0, 0, 0, 0}
 };
+
+static void usage(int status, const char *program_name)
+{
+	if (status != 0)
+		fprintf(stderr, "Wrong input parameters,"
+			" try %s --help' for more information.\n",
+			program_name);
+	else {
+		printf("Usage: %s [-h] [-g <controllers>:<path>] "
+			"[--sticky] command [arguments] ...\n",
+			program_name);
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -50,17 +65,9 @@ int main(int argc, char *argv[])
 	int c;
 	struct cgroup_group_spec *cgroup_list[CG_HIER_MAX];
 
-	if (argc < 2) {
-		fprintf(stderr, "Usage is %s"
-			" [-g <list of controllers>:<relative path to cgroup>]"
-			" [--sticky] command [arguments]  \n",
-			argv[0]);
-		exit(2);
-	}
-
 	memset(cgroup_list, 0, sizeof(cgroup_list));
 
-	while ((c = getopt_long(argc, argv, "+g:s", longopts, NULL)) > 0) {
+	while ((c = getopt_long(argc, argv, "+g:sh", longopts, NULL)) > 0) {
 		switch (c) {
 		case 'g':
 			ret = parse_cgroup_spec(cgroup_list, optarg,
@@ -75,16 +82,18 @@ int main(int argc, char *argv[])
 		case 's':
 			flag_child |= CGROUP_DAEMON_UNCHANGE_CHILDREN;
 			break;
+		case 'h':
+			usage(0, argv[0]);
+			exit(0);
 		default:
-			fprintf(stderr, "Invalid command line option\n");
+			usage(1, argv[0]);
 			exit(1);
-			break;
 		}
 	}
 
 	/* Executable name */
 	if (!argv[optind]) {
-		fprintf(stderr, "No command specified\n");
+		usage(1, argv[0]);
 		exit(1);
 	}
 
