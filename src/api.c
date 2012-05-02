196e9a65a6241b05ba99f12f998f9e3ae080d998
Ivana Hutarova Varekova <varekova@redhat.com>
2012-05-02 19:57:34 +0200

    api.c: remove obsolete error message
    Remove forgotten error message which break error code <-> message
    mapping
    
    Signed-off-by: Ivana Hutarova Varekova <varekova@redhat.com>
    Acked-by: Dhaval Giani <dhaval.giani@gmail.com>

--- src/api.c
+++ src/api.c
@@ -109,7 +109,6 @@ const char const *cgroup_strerror_codes[] = {
 	"Cgroup parsing failed",
 	"Cgroup, rules file does not exist",
 	"Cgroup mounting failed",
-	"The config file can not be opened",
 	"Sentinel",
 	"End of File or iterator",
 	"Failed to parse config file",
