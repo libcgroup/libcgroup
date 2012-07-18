4241205f257ba8eedb1b235787b2bdc2375245d8
Ivana Hutarova Varekova <varekova@redhat.com>
2012-07-18 21:02:45 +0200

    cgexec: more verbose help
    cgexec: create more verbose -h output similar as the other libcgroup tools
    
    
    Signed-off-by: Ivana Hutarova Varekova <varekova@redhat.com>
    Acked-by: Jan Safranek <jsafrane@redhat.com>

--- src/tools/cgexec.c
+++ src/tools/cgexec.c
@@ -50,6 +50,11 @@ static void usage(int status, const char *program_name)
 		printf("Usage: %s [-h] [-g <controllers>:<path>] "
 			"[--sticky] command [arguments] ...\n",
 			program_name);
+		printf("Run the task in given control groups\n");
+		printf("  -g <controllers>:<path>	Control "\
+			"group which should be added\n");
+		printf("  --sticky			cgred "\
+			"daemon does not change pidlist and children tasks\n");
 	}
 }
 
