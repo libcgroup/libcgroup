078f20ab1f27c08bd95c525dda8261dfcd6e0612
Tom Hromatka <tom.hromatka@oracle.com>
2020-11-03 08:04:20 -0700

    ftests: Add a cgroup v2 basic cgget test
    Add a test - 003-cgget-basic_cgget_v2.py - that tests
    cgget on a cgroup v2 system.
    
    -----------------------------------------------------------------
    Test Results:
            Run Date:                          Nov 03 17:49:35
            Passed:                                  1 test(s)
            Skipped:                                 0 test(s)
            Failed:                                  0 test(s)
    -----------------------------------------------------------------
    Timing Results:
            Test                                    Time (sec)
            ---------------------------------------------------------
            setup                                         3.76
            003-cgget-basic_cgget_v2.py                   0.70
            teardown                                      0.00
            ---------------------------------------------------------
            Total Run Time                                4.46
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

---
+++ ftests/003-cgget-basic_cgget_v2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+#
+# Basic cgget functionality test
+#
+# Copyright (c) 2020 Oracle and/or its affiliates.
+# Author: Tom Hromatka <tom.hromatka@oracle.com>
+#
+
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of version 2.1 of the GNU Lesser General Public License as
+# published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, see <http://www.gnu.org/licenses>.
+#
+
+from cgroup import Cgroup
+import consts
+import ftests
+import os
+import sys
+
+CONTROLLER='cpuset'
+CGNAME="003cgget"
+
+SETTING='cpuset.mems'
+VALUE='0'
+
+def prereqs(config):
+    result = consts.TEST_PASSED
+    cause = None
+
+    if Cgroup.version('cpuset') != Cgroup.CGROUP_V2:
+        result = consts.TEST_SKIPPED
+        cause = "This test requires the cgroup v2 cpuset controller"
+
+    return result, cause
+
+def setup(config):
+    Cgroup.create(config, CONTROLLER, CGNAME)
+    Cgroup.set(config, CGNAME, SETTING, VALUE)
+
+def test(config):
+    result = consts.TEST_PASSED
+    cause = None
+
+    value = Cgroup.get(config, controller=None, cgname=CGNAME,
+                       setting=SETTING, print_headers=False,
+                       values_only=True)
+
+    if value != VALUE:
+        result = consts.TEST_FAILED
+        cause = "cgget expected {} but received {}".format(VALUE, value)
+
+    return result, cause
+
+def teardown(config):
+    Cgroup.delete(config, CONTROLLER, CGNAME)
+
+def main(config):
+    [result, cause] = prereqs(config)
+    if result != consts.TEST_PASSED:
+        return [result, cause]
+
+    setup(config)
+    [result, cause] = test(config)
+    teardown(config)
+
+    return [result, cause]
+
+if __name__ == '__main__':
+    config = ftests.parse_args()
+    # this test was invoked directly.  run only it
+    config.args.num = int(os.path.basename(__file__).split('-')[0])
+    sys.exit(ftests.main(config))
