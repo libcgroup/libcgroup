8355bb35aaca79370ea55b9b87e00cda4d0bc3ca
Tom Hromatka <tom.hromatka@oracle.com>
2021-12-15 21:22:20 +0000

    libcgroup.so: Add a function to get the library version
    Add a function, cgroup_version(), that returns the current
    version of the libcgroup library.
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- src/libcgroup-internal.h
+++ src/libcgroup-internal.h
@@ -309,16 +309,6 @@ extern void cgroup_dictionary_iterator_end(void **handle);
 int cg_chmod_path(const char *path, mode_t mode, int owner_is_umask);
 
 /**
- * Get the cgroup version of a controller.  Version is set to CGROUP_UNK
- * if the version cannot be determined.
- *
- * @param controller The controller of interest
- * @param version The version of the controller
- */
-int cgroup_get_controller_version(const char * const controller,
-		enum cg_version_t * const version);
-
-/**
  * Build the path to the tasks or cgroup.procs file
  *
  * @param path Output variable that will contain the path.  Must be
