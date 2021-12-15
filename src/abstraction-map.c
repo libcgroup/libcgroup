a3b581d3e6dce6aca7db73bd6411220e526a0c01
Tom Hromatka <tom.hromatka@oracle.com>
2021-12-15 20:48:31 +0000

    abstraction-map: Add cpuset.effective_{cpus,mems} <-> cpuset.{cpus,mems}.effective mapping
    Add a name-only mapping to convert:
    	cpuset.effective_cpus <-> cpuset.cpus.effective
    	cpuset.effective_mems <-> cpuset.mems.effective
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- src/abstraction-map.c
+++ src/abstraction-map.c
@@ -35,6 +35,12 @@ const struct cgroup_abstraction_map cgroup_v1_to_v2_map[] = {
 	{cgroup_convert_int, "cpu.shares", (void *)1024, "cpu.weight", (void *)100},
 	{cgroup_convert_cpu_quota_to_max, "cpu.cfs_quota_us", NULL, "cpu.max", NULL},
 	{cgroup_convert_cpu_period_to_max, "cpu.cfs_period_us", NULL, "cpu.max", NULL},
+
+	/* cpuset controller */
+	{cgroup_convert_name_only, "cpuset.effective_cpus", NULL,
+		"cpuset.cpus.effective", NULL},
+	{cgroup_convert_name_only, "cpuset.effective_mems", NULL,
+		"cpuset.mems.effective", NULL},
 };
 const int cgroup_v1_to_v2_map_sz = sizeof(cgroup_v1_to_v2_map) /
 				   sizeof(cgroup_v1_to_v2_map[0]);
@@ -43,6 +49,12 @@ const struct cgroup_abstraction_map cgroup_v2_to_v1_map[] = {
 	{cgroup_convert_int, "cpu.weight", (void *)100, "cpu.shares", (void *)1024},
 	{cgroup_convert_cpu_max_to_quota, "cpu.max", NULL, "cpu.cfs_quota_us", NULL},
 	{cgroup_convert_cpu_max_to_period, "cpu.max", NULL, "cpu.cfs_period_us", NULL},
+
+	/* cpuset controller */
+	{cgroup_convert_name_only, "cpuset.cpus.effective", NULL,
+		"cpuset.effective_cpus", NULL},
+	{cgroup_convert_name_only, "cpuset.mems.effective", NULL,
+		"cpuset.effective_mems", NULL},
 };
 const int cgroup_v2_to_v1_map_sz = sizeof(cgroup_v2_to_v1_map) /
 				   sizeof(cgroup_v2_to_v1_map[0]);
