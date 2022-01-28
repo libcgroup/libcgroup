ee6977051426490934cd8e691de1e5a54a96f3e0
Tom Hromatka <tom.hromatka@oracle.com>
2022-01-28 10:21:10 -0700

    abstraction: Add mapping tables
    Add a table for cgroup v1 to v2 conversions -
    	cgroup_v1_to_v2_map[]
    
    Add a table for cgroup v2 to v1 conversions -
    	cgroup_v2_to_v1_map[]
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>
    Reviewed-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

---
+++ src/abstraction-map.c
@@ -0,0 +1,42 @@
+/**
+ * Libcgroup abstraction layer mappings
+ *
+ * Copyright (c) 2021-2022 Oracle and/or its affiliates.
+ * Author: Tom Hromatka <tom.hromatka@oracle.com>
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of version 2.1 of the GNU Lesser General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses>.
+ */
+
+#include <libcgroup.h>
+#include <libcgroup-internal.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "abstraction-common.h"
+#include "abstraction-map.h"
+
+const struct cgroup_abstraction_map cgroup_v1_to_v2_map[] = {
+};
+const int cgroup_v1_to_v2_map_sz = sizeof(cgroup_v1_to_v2_map) /
+				   sizeof(cgroup_v1_to_v2_map[0]);
+
+const struct cgroup_abstraction_map cgroup_v2_to_v1_map[] = {
+};
+const int cgroup_v2_to_v1_map_sz = sizeof(cgroup_v2_to_v1_map) /
+				   sizeof(cgroup_v2_to_v1_map[0]);
