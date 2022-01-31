1c73fe0fa5a79c9c243d073ce6e8214db002bf63
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2022-01-31 19:12:42 +0530

    api.c: add return value check for strdup()
    In function cg_build_path_locked(), check for return value from
    strdup(). This fixes, one of the long pending FIXME.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

--- src/api.c
+++ src/api.c
@@ -1512,9 +1512,10 @@ char *cg_build_path_locked(const char *name, char *path,
 
 			if (name) {
 				char *tmp;
-				tmp = strdup(path);
 
-				/* FIXME: missing OOM check here! */
+				tmp = strdup(path);
+				if (tmp == NULL)
+					break;
 
 				cg_concat_path(tmp, name, path);
 				free(tmp);
