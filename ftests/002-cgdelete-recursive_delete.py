2206ee41acd1b73b4917adf0a3f5f9309c120f5e
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2022-02-15 12:22:26 +0530

    test-002: make it PEP8 compliant
    Make test 002-cgdelete-recursive_delete.py PEP-8 compliant.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- ftests/002-cgdelete-recursive_delete.py
+++ ftests/002-cgdelete-recursive_delete.py
@@ -23,18 +23,19 @@
 from cgroup import Cgroup, CgroupVersion
 import consts
 import ftests
-import os
-from process import Process
 import sys
+import os
 
 CONTROLLER = 'cpuset'
 PARENT = '002cgdelete'
 CHILD = 'childcg'
 GRANDCHILD = 'grandchildcg'
 
+
 def prereqs(config):
     # This test should run on both cgroup v1 and v2
     return consts.TEST_PASSED, None
+
 
 def setup(config):
     Cgroup.create(config, CONTROLLER, PARENT)
@@ -49,16 +50,20 @@ def setup(config):
         # Moving a process from a child cgroup to its parent isn't (easily)
         # supported in cgroup v2 because of cgroup v2's restriction that
         # processes only be located in leaf cgroups
+        grandchild_cgrp_path = os.path.join(PARENT, CHILD, GRANDCHILD)
         config.process.create_process_in_cgroup(config, CONTROLLER,
-                                         os.path.join(PARENT, CHILD, GRANDCHILD))
+                                                grandchild_cgrp_path)
+
 
 def test(config):
     Cgroup.delete(config, CONTROLLER, PARENT, recursive=True)
 
     return consts.TEST_PASSED, None
 
+
 def teardown(config):
     pass
+
 
 def main(config):
     [result, cause] = prereqs(config)
@@ -70,6 +75,7 @@ def main(config):
     teardown(config)
 
     return [result, cause]
+
 
 if __name__ == '__main__':
     config = ftests.parse_args()
