aea452ee29cbd5a16d603cf1181387a8a21acce4
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2022-02-15 12:32:02 +0530

    test-010: make it PEP8 compliant
    Make test 010-cgget-g_flag_controller_and_cgroup.py PEP-8 compliant.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- ftests/010-cgget-g_flag_controller_and_cgroup.py
+++ ftests/010-cgget-g_flag_controller_and_cgroup.py
@@ -23,8 +23,8 @@
 from cgroup import Cgroup, CgroupVersion
 import consts
 import ftests
-import os
 import sys
+import os
 
 CONTROLLER = 'cpu'
 CGNAME = '010cgget'
@@ -53,20 +53,23 @@ cpu.uclamp.min: 0.00
 cpu.uclamp.max: max
 '''
 
+
 def prereqs(config):
     result = consts.TEST_PASSED
     cause = None
 
     return result, cause
 
+
 def setup(config):
     Cgroup.create(config, CONTROLLER, CGNAME)
+
 
 def test(config):
     result = consts.TEST_PASSED
     cause = None
 
-    out = Cgroup.get(config, controller="{}:{}".format(CONTROLLER, CGNAME),
+    out = Cgroup.get(config, controller='{}:{}'.format(CONTROLLER, CGNAME),
                      print_headers=False)
 
     version = CgroupVersion.get_version(CONTROLLER)
@@ -78,27 +81,38 @@ def test(config):
 
     if len(out.splitlines()) != len(expected_out.splitlines()):
         result = consts.TEST_FAILED
-        cause = "Expected line count: {}, but received line count: {}".format(
-                len(expected_out.splitlines()), len(out.splitlines()))
+        cause = (
+                    'Expected line count: {}, but received line count: {}'
+                    ''.format(len(expected_out.splitlines()),
+                              len(out.splitlines()))
+                )
         return result, cause
 
     if len(out.splitlines()) != len(expected_out.splitlines()):
         result = consts.TEST_FAILED
-        cause = "Expected {} lines but received {} lines".format(
-                len(expected_out.splitlines()), len(out.splitlines()))
+        cause = (
+                    'Expected {} lines but received {} lines'
+                    ''.format(len(expected_out.splitlines()),
+                              len(out.splitlines()))
+                )
         return result, cause
 
     for line_num, line in enumerate(out.splitlines()):
         if line.strip() != expected_out.splitlines()[line_num].strip():
             result = consts.TEST_FAILED
-            cause = "Expected line:\n\t{}\nbut received line:\n\t{}".format(
-                    expected_out.splitlines()[line_num].strip(), line.strip())
+            cause = (
+                        'Expected line:\n\t{}\nbut received line:\n\t{}'
+                        ''.format(expected_out.splitlines()[line_num].strip(),
+                                  line.strip())
+                    )
             return result, cause
 
     return result, cause
 
+
 def teardown(config):
     Cgroup.delete(config, CONTROLLER, CGNAME)
+
 
 def main(config):
     [result, cause] = prereqs(config)
@@ -110,6 +124,7 @@ def main(config):
     teardown(config)
 
     return [result, cause]
+
 
 if __name__ == '__main__':
     config = ftests.parse_args()
