9213865c76ca974482c7eff41c6c34df1bf75334
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2022-02-15 13:03:13 +0530

    test-041: make it PEP8 compliant
    Make test 041-pybindings-library_version.py PEP-8 compliant.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- ftests/041-pybindings-library_version.py
+++ ftests/041-pybindings-library_version.py
@@ -23,14 +23,17 @@
 from libcgroup import Cgroup
 import consts
 import ftests
-import os
 import sys
+import os
+
 
 def prereqs(config):
     return consts.TEST_PASSED, None
 
+
 def setup(config):
     return consts.TEST_PASSED, None
+
 
 def test(config):
     result = consts.TEST_PASSED
@@ -40,23 +43,25 @@ def test(config):
 
     if not isinstance(major, int):
         result = consts.TEST_FAILED
-        cause = "Major version failed. Received {}".format(major)
+        cause = 'Major version failed. Received {}'.format(major)
         return result, cause
 
     if not isinstance(minor, int):
         result = consts.TEST_FAILED
-        cause = "Minor version failed. Received {}".format(minor)
+        cause = 'Minor version failed. Received {}'.format(minor)
         return result, cause
 
     if not isinstance(release, int):
         result = consts.TEST_FAILED
-        cause = "Release version failed. Received {}".format(release)
+        cause = 'Release version failed. Received {}'.format(release)
         return result, cause
 
     return result, cause
 
+
 def teardown(config):
     pass
+
 
 def main(config):
     [result, cause] = prereqs(config)
@@ -68,6 +73,7 @@ def main(config):
     teardown(config)
 
     return [result, cause]
+
 
 if __name__ == '__main__':
     config = ftests.parse_args()
