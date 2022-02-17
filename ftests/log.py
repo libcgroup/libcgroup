c28cfa1959d82c5e8f8878ba49b6a9c13821c3d1
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2022-02-17 16:44:30 +0530

    log.py: make it PEP8 compliant
    Make log.py PEP-8 compliant.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- ftests/log.py
+++ ftests/log.py
@@ -19,8 +19,8 @@
 # along with this library; if not, see <http://www.gnu.org/licenses>.
 #
 
-import consts
 import datetime
+import consts
 import log
 
 log_level = consts.DEFAULT_LOG_LEVEL
@@ -37,22 +37,22 @@ class Log(object):
                 Log.open_logfd(log.log_file)
 
             timestamp = datetime.datetime.now().strftime('%b %d %H:%M:%S')
-            log_fd.write("{}: {}\n".format(timestamp, msg))
+            log_fd.write('{}: {}\n'.format(timestamp, msg))
 
     @staticmethod
     def open_logfd(log_file):
-        log.log_fd = open(log_file, "a")
+        log.log_fd = open(log_file, 'a')
 
     @staticmethod
     def log_critical(msg):
-        Log.log("CRITICAL: {}".format(msg), consts.LOG_CRITICAL)
+        Log.log('CRITICAL: {}'.format(msg), consts.LOG_CRITICAL)
 
     @staticmethod
     def log_warning(msg):
-        Log.log("WARNING: {}".format(msg), consts.LOG_WARNING)
+        Log.log('WARNING: {}'.format(msg), consts.LOG_WARNING)
 
     @staticmethod
     def log_debug(msg):
-        Log.log("DEBUG: {}".format(msg), consts.LOG_DEBUG)
+        Log.log('DEBUG: {}'.format(msg), consts.LOG_DEBUG)
 
 # vim: set et ts=4 sw=4:
