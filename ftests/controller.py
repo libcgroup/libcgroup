4fee920cc24094f10d367380ea4a568643743e3c
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2022-02-18 11:30:52 +0530

    controller.py: make it PEP8 compliant
    Make controller.py PEP-8 compliant.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- ftests/controller.py
+++ ftests/controller.py
@@ -21,6 +21,7 @@
 
 from log import Log
 
+
 class Controller(object):
     # The controller class closely mirrors libcgroup's struct cgroup_controller
     def __init__(self, name):
@@ -30,10 +31,11 @@ class Controller(object):
         self.settings = dict()
 
     def __str__(self):
-        out_str = "Controller {}\n".format(self.name)
+        out_str = 'Controller {}\n'.format(self.name)
 
         for setting_key in self.settings:
-            out_str += "    {} = {}\n".format(setting_key, self.settings[setting_key])
+            out_str += '    {} = {}\n'.format(setting_key,
+                                              self.settings[setting_key])
 
         return out_str
 
@@ -51,18 +53,27 @@ class Controller(object):
             added = other_keys - self_keys
             if added is not None:
                 for key in added:
-                    Log.log_critical("Other contains {} = {}".format(key, other.settings[key]))
+                    Log.log_critical(
+                                        'Other contains {} = {}'
+                                        ''.format(key, other.settings[key])
+                                    )
 
             removed = self_keys - other_keys
             if removed is not None:
                 for key in removed:
-                    Log.log_critical("Self contains {} = {}".format(key, self.settings[key]))
+                    Log.log_critical(
+                                        'Self contains {} = {}'
+                                        ''.format(key, self.settings[key])
+                                    )
 
             common = self_keys.intersection(other_keys)
             for key in common:
                 if self.settings[key] != other.settings[key]:
-                    Log.log_critical("self{} = {} while other{} = {}".format(
-                        key, self.settings[key], key, other.settings[key]))
+                    Log.log_critical(
+                                        'self{} = {} while other{} = {}'
+                                        ''.format(key, self.settings[key],
+                                                  key, other.settings[key])
+                                    )
 
             return False
 
