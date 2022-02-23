libcgroup/libcgroup

commit 498530a1d7447e689ed555170b6f8e20471317bc

cgcreate, tools: Allow cgroup creation with no controllers attached
Add support for creating a cgroup with no controllers attached.  This
is only supported on cgroup v2.

Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>
Reviewed-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

src/api.c
@@ -1485,15 +1485,19 @@ char *cg_build_path_locked(const char *name, char *path,
 {
 	int i, ret;
 	for (i = 0; cg_mount_table[i].name[0] != '\0'; i++) {
-		/* Two ways to successfully move forward here:
+		/* Three ways to successfully move forward here:
 		 * 1. The "type" controller matches the name of a mounted
 		 *    controller
 		 * 2. The "type" controller requested is "cgroup" and there's
 		 *    a "real" controller mounted as cgroup v2
+		 * 3. The "type" controller is NULL and there's a "real"
+		 *    controller mounted as cgroup v2.  This allows a user
+		 *    to create a v2 cgroup with no controllers enabled
 		 */
-		if ((strcmp(cg_mount_table[i].name, type) == 0) ||
-		    (strcmp(type, CGROUP_FILE_PREFIX) == 0 &&
-		     cg_mount_table[i].version == CGROUP_V2)) {
+		if ((type && strcmp(cg_mount_table[i].name, type) == 0) ||
+		    (type && strcmp(type, CGROUP_FILE_PREFIX) == 0 &&
+		     cg_mount_table[i].version == CGROUP_V2) ||
+		    (type == NULL && cg_mount_table[i].version == CGROUP_V2)) {
 			if (cg_namespace_table[i]) {
 				ret = snprintf(path, FILENAME_MAX, "%s/%s/",
 						cg_mount_table[i].mount.path,
@@ -2435,29 +2439,39 @@ static int _cgroup_create_cgroup(const struct cgroup * const cgroup,
 	fts_path[1] = NULL;
 	path = fts_path[0];
 
-	if (!cg_build_path(cgroup->name, path, controller->name))
-		goto err;
-
-	error = cgroup_get_controller_version(controller->name, &version);
-	if (error)
-		goto err;
-
-	if (version == CGROUP_V2) {
-		char *parent, *dname;
-
-		parent = strdup(path);
-		if (!parent) {
+	if (controller) {
+		if (!cg_build_path(cgroup->name, path, controller->name)) {
 			error = ECGOTHER;
 			goto err;
 		}
 
-		dname = dirname(parent);
-
-		error = cgroupv2_subtree_control_recursive(dname,
-				controller->name, true);
-		free(parent);
+		error = cgroup_get_controller_version(controller->name,
+						      &version);
 		if (error)
 			goto err;
+
+		if (version == CGROUP_V2) {
+			char *parent, *dname;
+
+			parent = strdup(path);
+			if (!parent) {
+				error = ECGOTHER;
+				goto err;
+			}
+
+			dname = dirname(parent);
+
+			error = cgroupv2_subtree_control_recursive(dname,
+					controller->name, true);
+			free(parent);
+			if (error)
+				goto err;
+		}
+	} else {
+		if (!cg_build_path(cgroup->name, path, NULL)) {
+			error = ECGOTHER;
+			goto err;
+		}
 	}
 
 	error = cg_create_control_group(path);
@@ -2488,9 +2502,11 @@ static int _cgroup_create_cgroup(const struct cgroup * const cgroup,
 	if (error)
 		goto err;
 
-	error = cgroup_set_values_recursive(base, controller, false);
-	if (error)
-		goto err;
+	if (controller) {
+		error = cgroup_set_values_recursive(base, controller, false);
+		if (error)
+			goto err;
+	}
 
 	if (!ignore_ownership && version == CGROUP_V1) {
 		error = cgroup_chown_chmod_tasks(base,
@@ -2530,6 +2546,14 @@ int cgroup_create_cgroup(struct cgroup *cgroup, int ignore_ownership)
 	for (i = 0; i < cgroup->index;	i++) {
 		if (!cgroup_test_subsys_mounted(cgroup->controller[i]->name))
 			return ECGROUPSUBSYSNOTMOUNTED;
+	}
+
+
+	if (cgroup->index == 0) {
+		/* create an empty cgroup v2 cgroup */
+		error = _cgroup_create_cgroup(cgroup, NULL, ignore_ownership);
+		if (error)
+			return error;
 	}
 
 	/*

src/abstraction-common.c
@@ -270,7 +270,14 @@ int cgroup_convert_cgroup(struct cgroup * const out_cgroup,
 			in_cgroup->controller[i]->version = in_version;
 		}
 
-		cgc->version = out_version;
+		if (strcmp(CGROUP_FILE_PREFIX, cgc->name) == 0)
+			/*
+			 * libcgroup only supports accessing cgroup.* files on
+			 * cgroup v2 filesystems.
+			 */
+			cgc->version = CGROUP_V2;
+		else
+			cgc->version = out_version;
 
 		if (cgc->version == CGROUP_UNK ||
 		    cgc->version == CGROUP_DISK) {

src/tools/tools-common.c
@@ -34,7 +34,7 @@ int parse_cgroup_spec(struct cgroup_group_spec **cdptr, char *optarg,
 {
 	struct cgroup_group_spec *ptr;
 	int i, j;
-	char *cptr, *pathptr, *temp;
+	char *cptr = NULL, *pathptr = NULL, *temp;
 
 	ptr = *cdptr;
 
@@ -52,13 +52,20 @@ int parse_cgroup_spec(struct cgroup_group_spec **cdptr, char *optarg,
 	}
 
 	/* Extract list of controllers */
-	cptr = strtok(optarg, ":");
-	cgroup_dbg("list of controllers is %s\n", cptr);
-	if (!cptr)
-		return -1;
+	if (optarg[0] == ':') {
+		/* No controller was passed in */
+		cptr = NULL;
+		pathptr = strtok(optarg, ":");
+	} else {
+		/* Extract the list of controllers from the user */
+		cptr = strtok(optarg, ":");
+		cgroup_dbg("list of controllers is %s\n", cptr);
+		if (!cptr)
+			return -1;
 
-	/* Extract cgroup path */
-	pathptr = strtok(NULL, ":");
+		pathptr = strtok(NULL, ":");
+	}
+
 	cgroup_dbg("cgroup path is %s\n", pathptr);
 	if (!pathptr)
 		return -1;
@@ -69,24 +76,27 @@ int parse_cgroup_spec(struct cgroup_group_spec **cdptr, char *optarg,
 		fprintf(stderr, "%s\n", strerror(errno));
 		return -1;
 	}
-	/* Convert list of controllers into an array of strings. */
-	j = 0;
-	do {
-		if (j == 0)
-			temp = strtok(cptr, ",");
-		else
-			temp = strtok(NULL, ",");
 
-		if (temp) {
-			cdptr[i]->controllers[j] = strdup(temp);
-			if (!cdptr[i]->controllers[j]) {
-				free(cdptr[i]);
-				fprintf(stderr, "%s\n", strerror(errno));
-				return -1;
+	if (cptr != NULL) {
+		/* Convert list of controllers into an array of strings. */
+		j = 0;
+		do {
+			if (j == 0)
+				temp = strtok(cptr, ",");
+			else
+				temp = strtok(NULL, ",");
+
+			if (temp) {
+				cdptr[i]->controllers[j] = strdup(temp);
+				if (!cdptr[i]->controllers[j]) {
+					free(cdptr[i]);
+					fprintf(stderr, "%s\n", strerror(errno));
+					return -1;
+				}
 			}
-		}
-		j++;
-	} while (temp && j<CG_CONTROLLER_MAX-1);
+			j++;
+		} while (temp && j<CG_CONTROLLER_MAX-1);
+	}
 
 	/* Store path to the cgroup */
 	strncpy(cdptr[i]->path, pathptr, FILENAME_MAX - 1);

commit 3625e85ff492463470a90cfcc61b80cc05165d46

api.c: Allow creation of a v2 cgroup with no controllers
Commit 498530a1d744 ("cgcreate, tools: Allow cgroup creation with
no controllers attached") attempted to add support to allow the
creation of a cgroup v2 cgroup with no controllers attached.  This
patch did create the requested cgroup, but it still required there
to be at least one valid controller _available_ to attach to.

Rework the aforementioned logic so that the cgroup can be created
if there's a valid cgroup v2 mount - even if there are no v2
controllers available.

Fixes: 498530a1d744 ("cgcreate, tools: Allow cgroup creation with no controllers attached")
Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>
Reviewed-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

src/api.c
@@ -88,6 +88,9 @@ static struct cgroup_rule_list trl;
 /* Lock for the list of rules (rl) */
 static pthread_rwlock_t rl_lock = PTHREAD_RWLOCK_INITIALIZER;
 
+/* Cgroup v2 mount path.  Null if v2 isn't mounted */
+static char cg_cgroup_v2_mount_path[FILENAME_MAX];
+
 /* Namespace */
 __thread char *cg_namespace_table[CG_CONTROLLER_MAX];
 
@@ -1195,6 +1198,12 @@ STATIC int cgroup_process_v2_mnt(struct mntent *ent, int *mnt_tbl_idx)
 	int ret = 0, i, duplicate;
 	FILE *fp = NULL;
 
+	/*
+	 * Save off this mount point.  This may be used later to build
+	 * the cg_path.
+	 */
+	strncpy(cg_cgroup_v2_mount_path, ent->mnt_dir, FILENAME_MAX);
+
 	/* determine what v2 controllers are available on this mount */
 	snprintf(cgroup_controllers_path, FILENAME_MAX, "%s/%s", ent->mnt_dir,
 		 CGV2_CONTROLLERS_FILE);
@@ -1304,6 +1313,8 @@ int cgroup_init(void)
 		}
 	}
 	memset(&cg_mount_table, 0, sizeof(cg_mount_table));
+
+	memset(&cg_cgroup_v2_mount_path, 0, sizeof(cg_cgroup_v2_mount_path));
 
 	proc_cgroup = fopen("/proc/cgroups", "re");
 
@@ -1484,25 +1495,46 @@ char *cg_build_path_locked(const char *name, char *path,
 			   const char *type)
 {
 	int i, ret;
+
+	/*
+	 * If no type is specified, and there's a valid cgroup v2 mount,
+	 * then build up a path to this mount (and cgroup name if supplied).
+	 * This can be used to create a cgroup v2 cgroup that's not attached
+	 * to any controller.
+	 */
+	if (!type && strlen(cg_cgroup_v2_mount_path) > 0) {
+		ret = snprintf(path, FILENAME_MAX, "%s/",
+			       cg_cgroup_v2_mount_path);
+		if (ret >= FILENAME_MAX)
+			cgroup_dbg("Warning: filename too long: %s/",
+				   cg_cgroup_v2_mount_path);
+		if (name) {
+			char *tmp;
+
+			tmp = strdup(path);
+			if (tmp == NULL)
+				return NULL;
+
+			cg_concat_path(tmp, name, path);
+			free(tmp);
+		}
+		return path;
+	}
+
 	for (i = 0; cg_mount_table[i].name[0] != '\0'; i++) {
-		/* Three ways to successfully move forward here:
+		/* Two ways to successfully move forward here:
 		 * 1. The "type" controller matches the name of a mounted
 		 *    controller
 		 * 2. The "type" controller requested is "cgroup" and there's
 		 *    a "real" controller mounted as cgroup v2
-		 * 3. The "type" controller is NULL and there's a "real"
-		 *    controller mounted as cgroup v2.  This allows a user
-		 *    to create a v2 cgroup with no controllers enabled
 		 */
 		if ((type && strcmp(cg_mount_table[i].name, type) == 0) ||
 		    (type && strcmp(type, CGROUP_FILE_PREFIX) == 0 &&
-		     cg_mount_table[i].version == CGROUP_V2) ||
-		    (type == NULL && cg_mount_table[i].version == CGROUP_V2)) {
+		     cg_mount_table[i].version == CGROUP_V2)) {
 			if (cg_namespace_table[i]) {
 				ret = snprintf(path, FILENAME_MAX, "%s/%s/",
 						cg_mount_table[i].mount.path,
 						cg_namespace_table[i]);
-				path[FILENAME_MAX-1] = '\0';
 				if (ret >= FILENAME_MAX)
 					cgroup_dbg("Warning: filename too long:"
 						"%s/%s/",
@@ -1511,7 +1543,6 @@ char *cg_build_path_locked(const char *name, char *path,
 			} else {
 				ret = snprintf(path, FILENAME_MAX, "%s/",
 						cg_mount_table[i].mount.path);
-				path[FILENAME_MAX-1] = '\0';
 				if (ret >= FILENAME_MAX)
 					cgroup_dbg("Warning: filename too long:"
 						"%s/",