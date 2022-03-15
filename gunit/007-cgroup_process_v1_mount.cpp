4b7c6c703a2209dca6b742ef2ab262cd72beabcd
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2022-03-15 16:39:27 +0530

    gunit: Adopt SPDX License tag
    Adopt SPDX license tag for all the source files, those already have
    LGPL 2.1 boilerplate in them and those missing license information.
    All the files in the project fall under project license, hence
    explicitly adding LGPL-2.1-only identifier to the files missing license
    information. Adopting SPDX license helps the compliance tools to
    determine the license and also helps in reducing the repetitive license
    boilerplate across source files.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- gunit/007-cgroup_process_v1_mount.cpp
+++ gunit/007-cgroup_process_v1_mount.cpp
@@ -1,22 +1,9 @@
+/* SPDX-License-Identifier: LGPL-2.1-only */
 /**
  * libcgroup googletest for cgroup_process_v1_mnt()
  *
  * Copyright (c) 2020 Oracle and/or its affiliates.
  * Author: Tom Hromatka <tom.hromatka@oracle.com>
- */
-
-/*
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of version 2.1 of the GNU Lesser General Public License as
- * published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, see <http://www.gnu.org/licenses>.
  */
 
 #include <mntent.h>
