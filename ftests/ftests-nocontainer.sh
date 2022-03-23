a8faad2c53d79e8a6e4052eed608d43b0f20a22c
Tom Hromatka <tom.hromatka@oracle.com>
2022-03-23 09:13:27 -0600

    ftests: Add support for make distcheck
    make distcheck builds libcgroup in a different directory
    from where it runs the tests.  Add smarts to ftests.sh and
    ftests-nocontainer.sh to support this case by copying the
    requisite python files.  After the tests complete, the
    python files are then removed.
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- ftests/ftests-nocontainer.sh
+++ ftests/ftests-nocontainer.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 # SPDX-License-Identifier: LGPL-2.1-only
 
-./ftests.py -l 10 -L ftests-nocontainer.log --no-container
+START_DIR=$PWD
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ "$START_DIR" != "$SCRIPT_DIR" ]; then
+	cp "$SCRIPT_DIR"/*.py "$START_DIR"
+fi
+
+if [ -d ../../src/python/build/lib.* ]; then
+	pushd ../../src/python/build/lib.*
+	export PYTHONPATH="$PYTHONPATH:$(pwd)"
+	popd
+fi
+
+./ftests.py -l 10 -L "$START_DIR/ftests-nocontainer.py.log" --no-container
+RET=$?
+
+if [ "$START_DIR" != "$SCRIPT_DIR" ]; then
+	rm -f "$START_DIR"/*.py
+	rm -fr "$START_DIR"/__pycache__
+	rm -f ftests-nocontainer.py.log
+fi
+
+exit $RET
