a8e27eb0bf6c9a15af9cbacc5247ea0f063b0b1f
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2022-03-25 11:36:21 +0530

    bootstrap.sh: make it shellcheck complaint
    Make bootstrap.in shellcheck complaint.  It fixes all the issues
    reported by the shellcheck version version 0.7.0.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 
 set -ex
 
-if [ -f .git/hooks/pre-commit.sample -a ! -f .git/hooks/pre-commit ] ; then
+if [ -f .git/hooks/pre-commit.sample ] && [ ! -f .git/hooks/pre-commit ] ; then
 	cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
 	chmod +x .git/hooks/pre-commit && \
 	echo "Activated pre-commit hook."
