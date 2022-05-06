2a32476a4eea296a7caafcea2a1c8b96adfca3c7
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2022-05-06 21:04:10 +0530

    gunit/012: replace controller name size definition
    Replace the struct cg_mount_table::name size definition FILENAME_MAX
    with CONTROL_NAMELEN_MAX.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- gunit/012-cgroup_create_cgroup.cpp
+++ gunit/012-cgroup_create_cgroup.cpp
@@ -59,7 +59,7 @@ class CgroupCreateCgroupTest : public ::testing::Test {
 		memset(&cg_namespace_table, 0, sizeof(cg_namespace_table));
 
 		for (i = 0; i < CONTROLLERS_CNT; i++) {
-			snprintf(cg_mount_table[i].name, FILENAME_MAX,
+			snprintf(cg_mount_table[i].name, CONTROL_NAMELEN_MAX,
 				 "%s", CONTROLLERS[i]);
 			cg_mount_table[i].version = VERSIONS[i];
 
