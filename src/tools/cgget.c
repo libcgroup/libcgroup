libcgroup/libcgroup

commit fea1ab8b45d7bbe708c9fd4e66b4d639fa7aa984

tools/cgget: fix coverity warning about resource leak
Fix two resource leaks reported by Coverity tool:

CID 1488718 (#1 of 1): Resource leak (RESOURCE_LEAK) leaked_storage:
Variable handle going out of scope leaks the storage it points to.

CID 1488723 (#1 of 1): Resource leak (RESOURCE_LEAK) leaked_storage:
Variable dir going out of scope leaks the storage it points to.

Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

src/tools/cgget.c
@@ -547,6 +547,9 @@ end:
 		cv->multiline_value = NULL;
 	}
 
+	if ((FILE *)handle)
+		fclose((FILE *)handle);
+
 	return ret;
 }
 
@@ -643,9 +646,10 @@ static int fill_empty_controller(struct cgroup * const cg,
 		}
 	}
 
-	closedir(dir);
-
 out:
+	if (dir)
+		closedir(dir);
+
 	pthread_rwlock_unlock(&cg_mount_table_lock);
 	return ret;
 }

src/api.c
@@ -4974,6 +4974,7 @@ int cgroup_read_value_end(void **handle)
 
 	fp = (FILE *)*handle;
 	fclose(fp);
+	*handle = NULL;
 
 	return 0;
 }

commit bb32160e3df5f410bfd15d5bbebd17e4af83389a

tools/cgget: fix coverity warning about double free
Fix double free warning, reported by Coverity tool:

CID 258297 (#1 of 1): Double free (USE_AFTER_FREE). double_free:
Calling closedir frees pointer dir which has already been freed.

As per the man page, the closedir(), closes the directory stream
associated with the dirp, but is ambiguous about if dirp is set to NULL
or not. Coverity answers that with the above report, that dirp is not
NULL and hence the double free.

Fixes: fea1ab8b45d7 ("tools/cgget: fix coverity warning about resource leak")
Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

src/tools/cgget.c
@@ -625,10 +625,8 @@ static int fill_empty_controller(struct cgroup * const cg,
 			continue;
 
 		ret = cgroup_fill_cgc(ctrl_dir, cg, cgc, i);
-		if (ret == ECGFAIL) {
-			closedir(dir);
+		if (ret == ECGFAIL)
 			goto out;
-		}
 
 		if (cgc->index > 0) {
 			cgc->values[cgc->index - 1]->dirty = false;