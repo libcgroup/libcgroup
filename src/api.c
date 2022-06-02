libcgroup/libcgroup

commit 9ce90c7edd281b359f67ee15b67fd28083ed4167

api.c: fix segfault in cgroup_populate_mount_points()
In cgroup_populate_mount_points(),  cgroup v1/v2 mount points get read
from /proc/mounts and populated into cg_mount_table[].  The size of
cg_mount_table[] is set to CG_CONTROLLER_MAX, that's defined as 100 and
if the system has more than CG_CONTROLLER_MAX, unique mount points, it
will segfault while processing them.  Fix this by checking, the mount
point count after processing every mount entry and bailout in case it
reaches CG_CONTROLLER_MAX.

The issue can be reproduced using, following simple bash commands on
cgroup v1:
1. sudo for i in $(seq 0 100); do sudo mkdir /name$i; done
2. sudo for i in $(seq 0 86); do sudo mount -t cgroup -o none,name=named$i
none /name$i; done
3. sudo cgget -g <controller>:<existing cgroup name>

Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

src/api.c
@@ -1466,6 +1466,9 @@ static int cgroup_populate_mount_points(char *controllers[CG_CONTROLLER_MAX])
 			if (ret)
 				goto err;
 
+			if (found_mnt >= CG_CONTROLLER_MAX)
+				break;
+
 			continue;
 		}
 
@@ -1481,11 +1484,19 @@ static int cgroup_populate_mount_points(char *controllers[CG_CONTROLLER_MAX])
 
 			if (ret)
 				goto err;
+
+			if (found_mnt >= CG_CONTROLLER_MAX)
+				break;
 		}
 	}
 
 	if (!found_mnt)
 		ret = ECGROUPNOTMOUNTED;
+
+	if (found_mnt >= CG_CONTROLLER_MAX) {
+		cgroup_err("Mount points exceeds CG_CONTROLLER_MAX");
+		ret = ECGMAXVALUESEXCEEDED;
+	}
 
 err:
 	if (proc_mount)

commit bd5e94a702cac6eeed623b67feedecbb91b218de

api.c: Fix handling of full cg_mount_table[]
Commit 9ce90c7edd28 ("api.c: fix segfault in cgroup_populate_mount_points()")
added logic to handle the case when there are 100+ cgroup mounts and
not overflow the cg_mount_table[].  But elsewhere in the libcgroup
code, it's expected that the last entry in the cg_mount_table[] has
a null name entry.

When the cg_mount_table[] is full, make the name of the last entry
null so that loops know to exit.  Also, add a couple bail out points
in cgroup_populate_mount_points() to ensure that we don't write
beyond the end of the table.

Depending upon the order in which the tests are run, this failure can
manifest itself as follows:
	$ cat tests/ftests/ftests-nocontainer.sh.log
	free(): invalid pointer
	./ftests-nocontainer.sh: line 18: 199390 Aborted
	(core dumped) ./ftests.py -l 10 -L "$START_DIR/ftests-nocontainer.py.log" --no-container -n Libcg"$RANDOM"
	FAIL ftests-nocontainer.sh (exit status: 134)

Fixes: 9ce90c7edd28 ("api.c: fix segfault in cgroup_populate_mount_points()")
Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

src/api.c
@@ -1172,6 +1172,9 @@ STATIC int cgroup_process_v1_mnt(char *controllers[], struct mntent *ent,
 		cgroup_cg_mount_table_append(controllers[i], ent->mnt_dir,
 					     CGROUP_V1, mnt_tbl_idx,
 					     ent->mnt_opts, shared_mnt);
+
+		if ((*mnt_tbl_idx) >= CG_CONTROLLER_MAX)
+			goto out;
 	}
 
 	/*
@@ -1325,6 +1328,9 @@ STATIC int cgroup_process_v2_mnt(struct mntent *ent, int *mnt_tbl_idx)
 		cgroup_cg_mount_table_append(controller, ent->mnt_dir,
 					     CGROUP_V2, mnt_tbl_idx,
 					     controller, shared_mnt);
+
+		if ((*mnt_tbl_idx) >= CG_CONTROLLER_MAX)
+			goto out;
 	} while ((controller = strtok_r(NULL, " ", &stok_buff)));
 
 out:
@@ -1498,6 +1504,11 @@ static int cgroup_populate_mount_points(char *controllers[CG_CONTROLLER_MAX])
 	if (found_mnt >= CG_CONTROLLER_MAX) {
 		cgroup_err("Mount points exceeds CG_CONTROLLER_MAX");
 		ret = ECGMAXVALUESEXCEEDED;
+		/*
+		 * There are loops in the libcgroup codebase that expect there
+		 * to be a null name entry at the end of the cg_mount_table[].
+		 */
+		cg_mount_table[found_mnt - 1].name[0] = '\0';
 	}
 
 err: