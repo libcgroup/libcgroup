libcgroup/libcgroup

commit 66799b867c7ace0affa61fba70fb0fab42725499

tools/cgxget: fix the resource leak in convert_cgroup()
Fix a resource leak warning reported by the Coverity tool:

CID 258272 (#1-2 of 2): Resource leak (RESOURCE_LEAK). leaked_storage:
Variable cg_converted_list going out of scope leaks the storage it
points to.

while free'ing() the cg_converted_list() via cgroup_free(), wrong array
index variable was passed, causing the resource leak. Fix it by passing
the right index variable.

Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

src/tools/cgxget.c
@@ -769,7 +769,7 @@ out:
 	if (ret != 0 && ret != ECGNOVERSIONCONVERT) {
 		/* The conversion failed */
 		for (j = 0; j < i; j++)
-			cgroup_free(&(cg_converted_list[i]));
+			cgroup_free(&(cg_converted_list[j]));
 	} else {
 		/*
 		 * The conversion succeeded or was unmappable.

commit 6a66c76c923d618935794ddff4e968bfc6b030b7

tools/cgxget: de-allocate cg_convert_list[] in convert_cgroup()
The commit 66799b867c7 (tools/cgxget: fix the resource leak in
convert_cgroup()), is a partial fix. It de-allocated the array members
of the dynamically allocated cg_converted_list[], but fails to free()
the cg_converted_list.  Fix the resource leak by de-allocating it.

Fixes: 66799b867c7 (tools/cgxget: fix the resource leak in convert_group)
Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

src/tools/cgxget.c
@@ -770,6 +770,7 @@ out:
 		/* The conversion failed */
 		for (j = 0; j < i; j++)
 			cgroup_free(&(cg_converted_list[j]));
+		free(cg_converted_list);
 	} else {
 		/*
 		 * The conversion succeeded or was unmappable.