libcgroup/libcgroup

commit d569122a8c5f9943e1842b5b776a8b1f84182f18

api.c: fix file open in cg_chmod_path()
In cg_chmod_path(), the commit 96db65fbb529 ("api.c: fix TOCTOU in
cg_chmod_path()), converted the file operations from stat -> fstat and
chmod -> fchmod to fix a Coverity warning.  The newly replaced file
operations operate on file descriptors and hence introduced a side
effect of opening the file at the wrong code block, that would only work
as expected when the caller calls cg_chmod_path() with owner_is_umask
set.

Fix it by moving the file operation out of the conditional block, so it
works in both of the cases of owner_is_umask being set or unset.

Fixes: 96db65fbb529 ("api.c: fix TOCTOU in cg_chmod_path())
Suggested-by: Tom Hromatka <tom.hromatka@oracle.com>
Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>
(cherry picked from commit 91cf2e4b7ceb19c02d66af717c18d7bc64fa5df9)

src/api.c
@@ -207,12 +207,12 @@ int cg_chmod_path(const char *path, mode_t mode, int owner_is_umask)
 	mode_t mask = -1U;
 	int fd;
 
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		goto fail;
+
 	if (owner_is_umask) {
 		mode_t umask, gmask, omask;
-
-		fd = open(path, O_RDONLY);
-		if (fd == -1)
-			goto fail;
 		/*
 		 * Use owner permissions as an umask for group and others
 		 * permissions because we trust kernel to initialize owner

commit 38f026747e433ba125eba8baa7940fb6a0f08256

api.c: fix resource leak in cg_chmod_path()
Fix resource leak, reported by Coverity Tool:

CID 276160 (#1 of 1): Resource leak (RESOURCE_LEAK)10. leaked_handle:
Handle variable fd going out of scope leaks the handle.

In cg_chmod_path(), the file descriptor checks for fd > 0, Coverity
warns about the possibility of the file descriptor being 0, fix it by
changing the check from '0' -> '-1'.

Fixes: d569122a8c5f ("api.c: fix file open in cg_chmod_path()")
Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

src/api.c
@@ -240,7 +240,7 @@ fail:
 			strerror(errno));
 	last_errno = errno;
 
-	if (fd > 0)
+	if (fd > -1)
 		close(fd);
 
 	return ECGOTHER;