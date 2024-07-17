8e058ccb675becf37c42097a8382e64061ed7a15
drakenclimber <drakenclimber@users.noreply.github.com>
2024-07-17 21:16:49 +0000

    deploy: e1a1b8fe1d9a08b6df0068a6711d1311b713e705

--- search/all_1e.js
+++ search/all_1e.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['x_20y_20branch_0',['8. If this is a new major/minor release, create new &apos;release-X.Y&apos; branch',['../md_doc_2internal_2release-github.html#autotoc_md16',1,'']]],
   ['xget_1',['xget',['../classftests_1_1cgroup_1_1Cgroup.html#a34cbac14b2e9f5ff1a63254298f9e0a6',1,'ftests::cgroup::Cgroup']]],
-  ['xset_2',['xset',['../classftests_1_1cgroup_1_1Cgroup.html#a7cd72ef09eae2778d15eb31f257c6058',1,'ftests::cgroup::Cgroup']]]
+  ['xset_2',['xset',['../classftests_1_1cgroup_1_1Cgroup.html#a80f6ba6c76930d3b0c06c1d92991ee56',1,'ftests::cgroup::Cgroup']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['xget_0',['xget',['../classftests_1_1cgroup_1_1Cgroup.html#a34cbac14b2e9f5ff1a63254298f9e0a6',1,'ftests::cgroup::Cgroup']]],
-  ['xset_1',['xset',['../classftests_1_1cgroup_1_1Cgroup.html#a7cd72ef09eae2778d15eb31f257c6058',1,'ftests::cgroup::Cgroup']]]
+  ['xset_1',['xset',['../classftests_1_1cgroup_1_1Cgroup.html#a80f6ba6c76930d3b0c06c1d92991ee56',1,'ftests::cgroup::Cgroup']]]
 ];
