391395c58da6bcea3703cb89072f1f1b235d3b49
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2024-08-11 11:56:23 +0530

    ftests/consts: Fix quote warnings
    Fix multiple "Q001 Single quote multiline found but double quotes
    preferred" warnings.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- tests/ftests/consts.py
+++ tests/ftests/consts.py
@@ -34,16 +34,16 @@ TEST_SKIPPED = 'skipped'
 CGRULES_FILE = '/etc/cgrules.conf'
 
 EXPECTED_CPU_OUT_V1 = [
-    '''cpu.cfs_period_us: 100000
+    """cpu.cfs_period_us: 100000
     cpu.stat: nr_periods 0
             nr_throttled 0
             throttled_time 0
     cpu.shares: 1024
     cpu.cfs_quota_us: -1
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # cfs_bandwidth without cpu.stat nr_busts, burst_time
-    '''cpu.cfs_burst_us: 0
+    """cpu.cfs_burst_us: 0
     cpu.cfs_period_us: 100000
     cpu.stat: nr_periods 0
             nr_throttled 0
@@ -52,9 +52,9 @@ EXPECTED_CPU_OUT_V1 = [
     cpu.idle: 0
     cpu.cfs_quota_us: -1
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # cfs_bandwidth with cpu.stat nr_busts, burst_time
-    '''cpu.cfs_burst_us: 0
+    """cpu.cfs_burst_us: 0
     cpu.cfs_period_us: 100000
     cpu.stat: nr_periods 0
             nr_throttled 0
@@ -65,9 +65,9 @@ EXPECTED_CPU_OUT_V1 = [
     cpu.idle: 0
     cpu.cfs_quota_us: -1
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # cfs_bandwidth with cpu.stat nr_busts, burst_time
-    '''cpu.cfs_burst_us: 0
+    """cpu.cfs_burst_us: 0
     cpu.cfs_period_us: 100000
     cpu.stat: nr_periods 0
             nr_throttled 0
@@ -79,11 +79,11 @@ EXPECTED_CPU_OUT_V1 = [
     cpu.stat.local: throttled_time 0
     cpu.cfs_quota_us: -1
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max'''
+    cpu.uclamp.max: max"""
 ]
 
 EXPECTED_CPU_OUT_V2 = [
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -94,9 +94,9 @@ EXPECTED_CPU_OUT_V2 = [
     cpu.pressure: some avg10=0.00 avg60=0.00 avg300=0.00 total=0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # with PSI
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -108,9 +108,9 @@ EXPECTED_CPU_OUT_V2 = [
             full avg10=0.00 avg60=0.00 avg300=0.00 total=0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # with PSI, cfs_bandwidth without cpu.stat nr_busts, burst_time
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -124,9 +124,9 @@ EXPECTED_CPU_OUT_V2 = [
     cpu.max.burst: 0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # with PSI, cfs_bandwidth with cpu.stat nr_busts, burst_time
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -142,9 +142,9 @@ EXPECTED_CPU_OUT_V2 = [
     cpu.max.burst: 0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # with PSI, cfs_bandwidth with cpu.stat nr_busts, burst_time, force_idle
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -161,10 +161,10 @@ EXPECTED_CPU_OUT_V2 = [
     cpu.max.burst: 0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # with PSI, cfs_bandwidth with cpu.stat nr_busts, burst_time, force_idle
     # cpu.stat.local
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -182,19 +182,19 @@ EXPECTED_CPU_OUT_V2 = [
     cpu.max.burst: 0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max'''
+    cpu.uclamp.max: max"""
 ]
 
 EXPECTED_PIDS_OUT = [
-        '''pids.current: 0
+        """pids.current: 0
         pids.events: max 0
         pids.max: max
-        ''',
-        '''pids.current: 0
+        """,
+        """pids.current: 0
         pids.events: max 0
         pids.max: max
         pids.peak: 0
-        '''
+        """
 ]
 
 # vim: set et ts=4 sw=4:
