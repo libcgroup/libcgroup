libcgroup/libcgroup

commit 0e50142a1fe0c2e5d3b80dc85c1b72764d27896c

api.c: Don't fail a recursive write if value isn't dirty
When cgroup_modify_cgroup() invokes cgroup_set_values_recursive(),
some of the settings within the cgroup may not be writable.  Avoid
failing the entire write by ignoring write failures on settings that
do not explicitly have the dirty flag set.

Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

src/api.c
@@ -1892,7 +1892,8 @@ static int cg_set_control_value(char *path, const char *val)
  * @param controller The controller whose values are being updated
  */
 STATIC int cgroup_set_values_recursive(const char * const base,
-	const struct cgroup_controller * const controller)
+	const struct cgroup_controller * const controller,
+	bool ignore_non_dirty_failures)
 {
 	char *path = NULL;
 	int error = 0, ret, j;
@@ -1912,6 +1913,15 @@ STATIC int cgroup_set_values_recursive(const char * const base,
 
 		free(path);
 		path = NULL;
+
+		if (error && ignore_non_dirty_failures &&
+		    !controller->values[j]->dirty) {
+			/* We failed to set this value, but it wasn't
+			 * marked as dirty, so ignore the failure.
+			 */
+			error = 0;
+			continue;
+		}
 
 		if (error)
 			goto err;
@@ -2142,7 +2152,7 @@ int cgroup_modify_cgroup(struct cgroup *cgroup)
 			continue;
 
 		error = cgroup_set_values_recursive(base,
-				cgroup->controller[i]);
+				cgroup->controller[i], true);
 		if (error)
 			goto err;
 	}
@@ -2393,7 +2403,7 @@ int cgroup_create_cgroup(struct cgroup *cgroup, int ignore_ownership)
 			goto err;
 
 		error = cgroup_set_values_recursive(base,
-				cgroup->controller[k]);
+				cgroup->controller[k], false);
 		if (error)
 			goto err;
 

src/libcgroup-internal.h
@@ -389,7 +389,8 @@ int cgroup_process_v1_mnt(char *controllers[], struct mntent *ent,
 int cgroup_process_v2_mnt(struct mntent *ent, int *mnt_tbl_idx);
 
 int cgroup_set_values_recursive(const char * const base,
-	const struct cgroup_controller * const controller);
+	const struct cgroup_controller * const controller,
+	bool ignore_non_dirty_failures);
 
 int cgroup_chown_chmod_tasks(const char * const cg_path,
 			     uid_t uid, gid_t gid, mode_t fperm);

commit d193a2eb7cf09fc6667ce6fab9dda4f100fc1ca1

api.c: fix memory leak on non-writable settings
When hitting the path st_mode check, we might already have allocated a
path. In this case, the continue re-enters the loop without freeing the
path:

 653 bytes in 8 blocks are definitely lost in loss record
    at 0x484582F: realloc (vg_replace_malloc.c:1437)
    by 0x49023E1: __vasprintf_internal (vasprintf.c:79)
    by 0x48D5BD5: asprintf (asprintf.c:31)
    by 0x4B41722: cgroup_set_values_recursive (api.c:2414)
    by 0x4B4688A: _cgroup_create_cgroup (api.c:2999)
    by 0x4B47067: cgroup_create_cgroup (api.c:3062)
    by 0x4B47067: cgroup_create_cgroup (api.c:3028)

Fixes: 0e50142a1fe0 ("api.c: Don't fail a recursive write if value isn't dirty")

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/api.c
@@ -2427,8 +2427,11 @@ STATIC int cgroup_set_values_recursive(const char * const base,
 		}
 
 		/* 0200 == S_IWUSR */
-		if (!(path_stat.st_mode & 0200))
+		if (!(path_stat.st_mode & 0200)) {
+			free(path);
+			path = NULL;
 			continue;
+		}
 
 		cgroup_dbg("setting %s to \"%s\", pathlen %d\n", path, cv->value, ret);
 