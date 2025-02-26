9c8ce4de5381aa29864523a2d26a46f9594b341b
Tom Hromatka <tom.hromatka@oracle.com>
2025-02-26 19:48:13 +0000

    abstraction-map: Add memory controller abstractions
    Add abstraction logic for:
    	memory.max <-> memory.limit_in_bytes
    	memory.high <-> memory.soft_limit_in_bytes
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

--- src/abstraction-common.h
+++ src/abstraction-common.h
@@ -112,6 +112,23 @@ int cgroup_convert_cpuset_to_partition(struct cgroup_controller * const dst_cgc,
 				       const char * const in_value, const char * const out_setting,
 				       void *in_dflt, void *out_dflt);
 
+/* memory */
+int cgroup_convert_memory_limit_to_max(struct cgroup_controller * const dst_cgc,
+				const char * const in_value, const char * const out_setting,
+				void *in_dflt, void *out_dflt);
+
+int cgroup_convert_memory_soft_limit_to_max(struct cgroup_controller * const dst_cgc,
+				const char * const in_value, const char * const out_setting,
+				void *in_dflt, void *out_dflt);
+
+int cgroup_convert_memory_max_to_limit(struct cgroup_controller * const dst_cgc,
+				const char * const in_value, const char * const out_setting,
+				void *in_dflt, void *out_dflt);
+
+int cgroup_convert_memory_high_to_soft_limit(struct cgroup_controller * const dst_cgc,
+				const char * const in_value, const char * const out_setting,
+				void *in_dflt, void *out_dflt);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
