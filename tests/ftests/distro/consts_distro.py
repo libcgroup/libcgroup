eecaedb3a46af71866c4e5060601481c96a947de
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2025-07-16 13:02:53 +0000

    ftests/utils: Move get_distro() to distro/consts_distro.py
    get_distro() helper was introduced to choose the right cpu controller
    settings list, based on the underlying distribution. Now that, the
    consts are moved to the consts directory, move the helper also to the
    distro/consts_distro.py file. Also, add the logic to raise an Expection
    when unsupported distro found.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

--- tests/ftests/distro/consts_distro.py
+++ tests/ftests/distro/consts_distro.py
@@ -10,6 +10,19 @@
 
 from distro import consts_ubuntu
 
+
+# get the current linux flavour
+def get_distro(config):
+    with open('/etc/os-release', 'r') as relfile:
+        buf = relfile.read()
+        if "Oracle Linux" in buf:
+            return "oracle"
+        elif "Ubuntu" in buf:
+            return "ubuntu"
+        else:
+            raise NotImplementedError("Unsupported Distro")
+
+
 consts = consts_ubuntu.ConstsUbuntu()
 
 # vim: set et ts=4 sw=4:
